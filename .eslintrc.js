9b0267d279ebb0e1933a3b4c4eae31f9e96904de
ozkersemih <huseyinflash@gmail.com>
2024-08-28 10:22:28 +0300

    update packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin',
-    'nest-swagger-checker-lint'
+    '@trendyol/eslint-plugin-nest-swagger-checker-lint'
   ],
   extends: [
     'plugin:@typescript-eslint/recommended',
@@ -19,9 +19,9 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    'nest-swagger-checker-lint/api-property-rule': 'error',
-    "nest-swagger-checker-lint/api-information-rule": "error",
-    "nest-swagger-checker-lint/api-param-rule": "error",
+    '@trendyol/nest-swagger-checker-lint/api-property-rule': 'error',
+    "@trendyol/nest-swagger-checker-lint/api-information-rule": "error",
+    "@trendyol/nest-swagger-checker-lint/api-param-rule": "error",
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
